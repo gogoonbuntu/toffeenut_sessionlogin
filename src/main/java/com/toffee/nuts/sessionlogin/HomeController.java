f6d090e68f95a41a3f83035722bbbb7eebf52702
gogoonbuntu <tmddud333@naver.com>
2023-06-01 03:13:28 +0900

    Initial commit

---
+++ src/main/java/com/toffee/nuts/sessionlogin/HomeController.java
@@ -0,0 +1,19 @@
+package com.toffee.nuts.sessionlogin;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        log.info("heheh");
+        return "home";
+    }
+}
---
+++ src/test/java/com/toffee/nuts/sessionlogin/SessionloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toffee.nuts.sessionlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
